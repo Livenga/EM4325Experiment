3a0fbf8110053b4d7f02d1c34aba3cfc081608e8
Jin Okura <ljmmz0405@gmail.com>
2022-03-08 17:14:55 +0900

    changed: 微修正

--- Controller/ctrl4325/src/systick.c
+++ Controller/ctrl4325/src/systick.c
@@ -1,16 +1,18 @@
 #include <stdint.h>
 #include "../../libstm32l0/include/libstm32l0.h"
 
-extern void mdelay16(uint16_t msec);
 
 static uint8_t _is_bright = 0;
 
 
 void SysTick_handler(void) {
+#if 1
   if(_is_bright) {
     GPIOA->BSRR = (1 <<  26);
   } else {
     GPIOA->BSRR = (1 <<  10);
   }
+#endif
+
   _is_bright ^= 1;
 }
